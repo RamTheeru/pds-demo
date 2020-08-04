d95069e54b09014836ec21677a723d30b2d7b7b5
RamTheeru <theertha808@gmail.com>
2020-08-04 21:37:39 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {Routes,RouterModule,PreloadAllModules} from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { PdsMainComponent } from './pds-main/pds-main.component';
 import { HomeComponent } from './home/home.component';
 
+
+const appRoutes : Routes=[
+   //{path :'',redirectTo : '/recipes',pathMatch :'full'},
+{path :'',component : HomeComponent},
+
+   {path :'login',component : PdsMainComponent}
+
+];
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(appRoutes,{preloadingStrategy:PreloadAllModules}) ],
   declarations: [ AppComponent, PdsMainComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
