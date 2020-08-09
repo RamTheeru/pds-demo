25084d517c4c109416b04981e11a22da3ac6dbd7
RamTheeru <theertha808@gmail.com>
2020-08-09 20:12:16 +0530

    1 file modified

--- src/app/scrolling/scrolling.component.ts
+++ src/app/scrolling/scrolling.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit,Input } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+//import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-scrolling',
   templateUrl: './scrolling.component.html',
-  styleUrls: ['./scrolling.component.css'],
-  animations: [
-    trigger('scroll', [
-      state('on', style({left: '-100px'})),
-      transition('* => *', [
-        style({left: '-100px'}),
-        animate(10000, style({left: '100%'}))
-      ])
-    ])
-  ]
+  styleUrls: ['./scrolling.component.css']
+  // animations: [
+  //   trigger('scroll', [
+  //     state('on', style({left: '-100px'})),
+  //     transition('* => *', [
+  //       style({left: '-100px'}),
+  //       animate(10000, style({left: '100%'}))
+  //     ])
+  //   ])
+  // ]
 })
 export class ScrollingComponent implements OnInit {
  @Input('txt') text :string ='';
