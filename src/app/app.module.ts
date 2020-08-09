0787f7573abe0265bf41533f9794dd2b21dd6afc
RamTheeru <theertha808@gmail.com>
2020-08-09 20:07:56 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {Routes,RouterModule,PreloadAllModules} from '@angular/router';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {appRoutingModule} from './app-routing.module';
 //import {HttpClientModule} from "@angular/common/http";
 
