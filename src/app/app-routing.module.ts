189614d37a0fa9e4d52f64f2ae4afcf83203c1c3
RamTheeru <theertha808@gmail.com>
2020-08-12 13:08:43 +0530

    3 files modified, 3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {Routes,RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {PdsMainComponent} from './pds-main/pds-main.component';
 import {AboutusComponent} from './aboutus/aboutus.component';
+import {RegisterComponent} from './register/register.component';
 const appRoutes : Routes =[
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'home',component:HomeComponent },
   {path:'login',component:PdsMainComponent},
-  {path:'aboutus',component:AboutusComponent}
+  {path:'aboutus',component:AboutusComponent},
+  {path:'register',component:RegisterComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,13 @@ import { LoadingComponent } from './loading/loading.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { LogintabComponent } from './logintab/logintab.component';
 import { ScrollingComponent } from './scrolling/scrolling.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,appRoutingModule ],
-  declarations: [ AppComponent, PdsMainComponent, HomeComponent, LoadingComponent, AboutusComponent, LogintabComponent, ScrollingComponent ],
+  declarations: [ AppComponent, PdsMainComponent, HomeComponent, LoadingComponent, AboutusComponent, LogintabComponent, ScrollingComponent, RegisterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
