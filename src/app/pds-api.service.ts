1275c6605086362175bcd25e82b200077d6ccc96
RamTheeru <theertha808@gmail.com>
2020-08-24 19:34:04 +0530

    1 file modified

--- src/app/pds-api.service.ts
+++ src/app/pds-api.service.ts
@@ -6,24 +6,24 @@ export class PdsApiService {
 
 
 
-   // Base url
-   baseurl = 'http://35.153.184.145/api/Employee/';
-  //baseurl='http://localhost:44302/api/Employee/';
+//    // Base url
+//    baseurl = 'http://35.153.184.145/api/Employee/';
+//   //baseurl='http://localhost:44302/api/Employee/';
 
-   userTypesUrl : string = 'UserTypes';
-     constructor(private http: HttpClient) { }
-      httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-        //'X-Requested-With': 'XMLHttpRequest',
-      'access-Control-Allow-Origin': '*'
-       //'access-Control-Allow-Methods:':'GET,POST,OPTIONS,DELETE,PUT'
-    })
-  }
-  getUserTypes() :  R.Observable<any>  {
-    console.log(this.baseurl + this.userTypesUrl);
-  return this.http.get(this.baseurl + this.userTypesUrl,this.httpOptions)
+//    userTypesUrl : string = 'UserTypes';
+//      constructor(private http: HttpClient) { }
+//       httpOptions = {
+//     headers: new HttpHeaders({
+//       'Content-Type': 'application/json',
+//         //'X-Requested-With': 'XMLHttpRequest',
+//       'access-Control-Allow-Origin': '*'
+//        //'access-Control-Allow-Methods:':'GET,POST,OPTIONS,DELETE,PUT'
+//     })
+//   }
+//   getUserTypes() :  R.Observable<any>  {
+//     console.log(this.baseurl + this.userTypesUrl);
+//   return this.http.get(this.baseurl + this.userTypesUrl,this.httpOptions)
 
-}
+// }
 
 }
